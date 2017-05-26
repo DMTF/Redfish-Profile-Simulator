03b2480435f7e8542bc1ed8415222d9d84ec2d35
Michael E Brown <michael_e_brown@dell.com>
2017-05-26 12:40:33 -0500

    Update mkvenv.sh to work cleanly on macos or fedora
    Signed-off-by: Michael E Brown <michael_e_brown@dell.com>

--- mkvenv.sh
+++ mkvenv.sh
@@ -1,4 +1,45 @@
 #!/bin/sh
 
-virtualenv ./venv
-. ./venv/bin/activate
+if [ -n "$BASH_SOURCE" ]; then
+    THIS_SCRIPT=$BASH_SOURCE
+elif [ -n "$ZSH_NAME" ]; then
+    THIS_SCRIPT=$0
+else
+    THIS_SCRIPT="$(pwd)/mkvenv.sh"
+fi
+
+
+scriptdir=$(cd $(dirname $THIS_SCRIPT); pwd)
+venv=$scriptdir/venv
+unset THIS_SCRIPT
+
+create_venv() {
+    local venv=$1
+    if [ -z "$VIRTUALENV" ]; then
+        local possible
+        for possible in virtualenv-3 virtualenv
+        do
+            local try=$(which $possible)
+            if [ -e $try ]; then
+                local VIRTUALENV=$try
+                break
+            fi
+        done
+    fi
+
+    if [ -z "$VIRTUALENV" ]; then
+        echo "Could not find virtualenv on the path"
+    else
+        $VIRTUALENV $venv
+    fi
+}
+
+if [ ! -d $venv ]; then
+    create_venv $venv
+fi
+
+if [ -d $venv ]; then
+    . $venv/bin/activate
+else
+    echo "No virtual environment available to activate"
+fi
