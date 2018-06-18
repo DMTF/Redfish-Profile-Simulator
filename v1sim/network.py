99e13f2e92d1bc5adf994b88cde3b3fe7ec6c7c3
Rene Peinthor <rene.peinthor@linbit.com>
2018-06-18 07:41:41 +0200

    correctly initialize RfEthernetCollection

--- v1sim/network.py
+++ v1sim/network.py
@@ -12,12 +12,19 @@ class RfEthernetCollection(RfCollection):
         return RfEthernet
 
     def create_sub_objects(self, base_path, rel_path):
+        self.elements = {}
         resource_path = os.path.join(base_path, rel_path);
         contents = os.listdir(resource_path)
         for item in contents:
             if item == "VLANs":
                 self.components[item] = RfVLanCollection(base_path, os.path.join(rel_path, item), parent=self)
-
+            else:
+                item_path = os.path.join(resource_path, item)
+                if os.path.isdir(item_path):
+                    etype = self.element_type()  # type: Type[RfEthernetCollection]
+                    self.elements[item] = etype(base_path,
+                                                os.path.normpath("%s/%s" % (rel_path, item)),
+                                                parent=self)
 
 class RfEthernet(RfResource):
     pass
